193afca62c33d172e53373eece791ac49203f26a
trieu04 <115911417+trieu04@users.noreply.github.com>
2024-10-12 00:24:58 +0700

    update env config

--- src/config/env.client.ts
+++ src/config/env.client.ts
@@ -1,10 +1,17 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
+export let envError: Error | null = null;
+
 export const env = createEnv({
-  clientPrefix: "VITE_PUBLIC_",
+  clientPrefix: "VITE_",
   client: {
-    VITE_PUBLIC_HELLO: z.string().optional(),
+    VITE_APP_API_URL: z.string().url(),
   },
   runtimeEnv: import.meta.env,
+  onValidationError(error) {
+    envError = error;
+    console.error(error);
+    return null as never;
+  }
 });
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
-import './config/env.client';
+import { ConfigurationError } from './components/configuration-error';
+import { envError } from './config/env.client';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const root = createRoot(document.getElementById('root')!);
+
+if (envError) {
+  root.render(
+    <ConfigurationError
+      message="Environment configuration error"
+    />
+  );
+  throw envError;
+}
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
