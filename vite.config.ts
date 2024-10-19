a810c2b7c23fa2bc0867b1d55317a402242e9ca8
trieu04 <115911417+trieu04@users.noreply.github.com>
2024-10-20 03:28:08 +0700

    update vite config - add custom server port by env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
+/// <reference types="vite/client" />
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  envDir: './env',
-  root: './'
-})
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig(({ mode }) => {
+  const envDir = './env';
+  const env = loadEnv(mode, envDir);
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    envDir: envDir,
+    root: './',
+    server: {
+      port: Number(env.VITE_APP_PORT) || undefined,
+    },
+  };
+});
