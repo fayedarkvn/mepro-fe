aaf230a3ff069e0a5c51be0d1a5e5f045c0d04d0
trieu04 <115911417+trieu04@users.noreply.github.com>
2025-06-07 21:41:44 +0700

    chore: migrate ESLint configuration to new format and add package manager info

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu({
+  type: 'app',
+  stylistic: {
+    semi: true,
+    indent: 2,
+    quotes: 'single',
+  },
+  imports: true,
+  unicorn: true,
+  typescript: {
+    overrides: {
+      'ts/consistent-type-imports': 'off',
+      'perfectionist/sort-imports': 'off',
+      'perfectionist/sort-named-imports': 'off',
+    },
+  },
+  react: {
+    overrides: {
+      'react-refresh/only-export-components': 'off',
+      'react/no-array-index-key': 'off',
+      'react-dom/no-missing-button-type': 'off',
+    },
+  },
+  javascript: true,
+  jsonc: false,
+  markdown: false,
+  toml: false,
+  ignores: [],
+});
